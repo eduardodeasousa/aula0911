9505b63b7e089416a6d8d9bd3d29bbd5249bd231
11910875651 <11910875651@lab06m33.nrc.ice.ufjf.br>
2016-11-09 20:32:54 -0200

    Inicial

---
+++ src/aula09/pkg11/Aula0911.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula09.pkg11;
+
+/**
+ *
+ * @author 11910875651
+ */
+public class Aula0911 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JanelaTabela nova = new JanelaTabela();
+        nova.setVisible(true);
+    }
+    
+}
